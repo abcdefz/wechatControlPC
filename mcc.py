68f325eb07c7955165e1a9c6f22a8e677ded94da
abcdefz <ztz_sjtu@163.com>
2016-08-04 19:36:07 +0800

    mcc.py

---
+++ mcc.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+import time
+import sys
+from utils.mailHelper import mailHelper
+from utils.excutor import executor
+from utils.configReader import configReader
+
+__Author__ = 'ztz'
+__Version__ = 0.5
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+class MCC(object):
+    CONFIGPATH = '_config.ini'
+    KEY_COMMAND = 'Command'
+    KEY_OPEN = 'Open'
+    KEY_BOSS = 'Boss'
+    KEY_TIMELIMIT = 'timelimit'
+
+    def __init__(self):
+        self.mailHelper = mailHelper()
+        self.configReader = configReader(self.CONFIGPATH)
+        commandDict = self.configReader.getDict(self.KEY_COMMAND)
+        openDict = self.configReader.getDict(self.KEY_OPEN)
+        self.timeLimit = int(self.configReader.readConfig(self.KEY_BOSS, self.KEY_TIMELIMIT))
+        self.executor = executor(commandDict, openDict)
+        self.toRun()
+
+    def toRun(self):
+        while True:
+            self.run()
+            time.sleep(self.timeLimit)
+
+    def run(self):
+        mailBody = self.mailHelper.acceptMail()
+        if mailBody:
+            exe = self.mailHelper.analysisMail(mailBody)
+            if exe:
+                self.executor.execute(exe)
+
+if __name__ == '__main__':
+    mcc = MCC()
